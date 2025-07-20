683bec8538d3657ec6f291a8a3fab502eb4e1975
Diego <diegodecstraid@gmail.com>
2025-07-20 13:23:05 -0300

    Initial commit

---
+++ src/main/java/me/diego/DevJavaBancarioApplication.java
@@ -0,0 +1,13 @@
+package me.diego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevJavaBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevJavaBancarioApplication.class, args);
+	}
+
+}
